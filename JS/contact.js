995c9d79d5d4113d0e9e2bc40236def8dc433379
EliasGrinwis <r0892566@student.thomasmore.be>
2022-04-14 13:13:14 +0200

    Updated

--- JS/contact.js
+++ JS/contact.js
@@ -1,11 +1,2 @@
 
-let button = document.getElementById('button-form');
-let username = document.getElementById('naam');
-console.log(username)
-
-button.addEventListener('click', function () {
-    button.style.backgroundColor = 'green';
-    button.style.fontSize = "large";
-    button.innerHTML = 'Verzonden'
-})
 
